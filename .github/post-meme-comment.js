4139b9223dfb4eb7303854cba456c2e9450794e6
DahVik <118915855+DahVik@users.noreply.github.com>
2023-05-21 22:26:38 +0530

    Update post-meme-comment.js

--- .github/post-meme-comment.js
+++ .github/post-meme-comment.js
@@ -2,17 +2,8 @@ const { Octokit } = require("@octokit/rest");
 
 async function run() {
   try {
-    const octokit = new Octokit({ auth: process.env.GITHUB_TOKEN });
-
-    const issueNumber = process.env.GITHUB_EVENT.issue.number;
-    const commentBody = "Here's the meme image: ![Image](https://raw.githubusercontent.com/DahVik/SubTask3/main/$.\SubTask3\meme1.jpg)";
-
-    await octokit.issues.createComment({
-      owner: process.env.GITHUB_REPOSITORY_OWNER,
-      repo: process.env.GITHUB_REPOSITORY,
-      issue_number: issueNumber,
-      body: commentBody,
-    });
+    const octokit = new Octokit();
+    console.log(octokit);
   } catch (error) {
     console.error(error);
     process.exit(1);
@@ -20,3 +11,4 @@ async function run() {
 }
 
 run();
+
